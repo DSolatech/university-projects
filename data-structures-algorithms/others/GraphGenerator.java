54ad926013bc96f58d6cbd551df6a46cae0c7348
DSolatech <danielsolamvl@gmail.com>
2026-08-10 17:32:08 +0200

    feat: Add new class GrahGenerator to test Prim algorithm

--- data-structures-algorithms/others/GraphGenerator.java
+++ data-structures-algorithms/others/GraphGenerator.java
@@ -6,5 +6,40 @@ public class GraphGenerator{
 
     public static void main(String[] args){
         
+        if(args.length != 3){
+            System.err.println("Use: java GraphGenerator <n> <y> <text_file>");
+            return;
+        }
+
+        int n = 0;
+        int y = 0;
+        String fileName = args[2];
+
+        try {
+            n = Integer.parseInt(args[0]);
+            y = Integer.parseInt(args[1]);
+
+        } catch (NumberFormatException e){
+            System.err.println("Error: Invalid integers");
+            return;
+        }
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+            writer.println(n + " " + y);
+
+            for (int i = 1; i <= n; i++){
+                for(int j = i + 1; j <= n; j++){
+                    int cost = (i*j) % y;
+                    writer.println(i + " " + j + " " + cost);
+                }
+            }
+            System.out.println("File: " + fileName + " was created!");
+
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
     }
+
+
 }
